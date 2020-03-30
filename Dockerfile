15664d256146f7370e5093a3a7ce79a4992d96d6
me <23647018+phisv@users.noreply.github.com>
2020-03-30 11:42:07 -0500

    updated action

--- Dockerfile
+++ Dockerfile
@@ -1,3 +1,4 @@
 FROM alpine:3.10
 COPY entrypoint.sh /entrypoint.sh
+COPY phrases.txt /phrases.txt
 ENTRYPOINT ["/entrypoint.sh"]
--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,9 @@
 #!/bin/sh -l
 
-echo "OwO it's $1!"
 time=$(date)
+n=$RANDOM
+
+IFS=$'\n' read -d '' -r -a arr < ./phrases.txt
+
+echo "${arr[n%${#arr[*]}]}, $1!"
 echo "::set-output name=time::$time"
