df179deea7def65202ca8a092c77c8788985e13c
Bella Xu <23647018+phisv@users.noreply.github.com>
2020-03-30 11:47:18 -0500

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,7 +3,7 @@
 time=$(date)
 n=$RANDOM
 
-IFS=$'\n' read -d '' -r -a arr < ./phrases.txt
+IFS=$'\n' read -d '' -r -a arr < phrases.txt
 
 echo "${arr[n%${#arr[*]}]}, $1!"
 echo "::set-output name=time::$time"
